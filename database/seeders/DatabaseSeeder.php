40b85c4151178d9b8bf002fec19c0bf439872d74
Chris Joe <chris.joe@soulmachines.com>
2021-02-06 23:40:28 +1300

    fix: tweak seeder to be ready for filling in database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Spatie\SimpleExcel\SimpleExcelReader;
+use Carbon\Carbon;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,13 +15,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // TODO: look for a better to form a relative path name
+        // TODO: look for a better way to form a relative path name
         $movementPath = dirname(__FILE__) . '/sources/Fertiliser inventory movements - Sheet1.csv';
         $rows = SimpleExcelReader::create($movementPath)->getRows();
 
-        $rows->each(function(array $rowProperties) {
-            print_r($rowProperties);
-        // \App\Models\User::factory(10)->create();
+        $data = $rows->map(function(array $props) {
+            // TODO: maybe use Carbon::createFromFormat() in future
+            // to make sure DateTime understands the format is d/m/y format
+            $date = new Carbon(str_replace('/', '-', $props['Date']));
+
+            return [
+                'date' => $date,
+                'type' => $props['Type'],
+                'quantity' => $props['Quantity'],
+                'unit_price' => $props['Unit Price'],
+            ];
         });
+
+        print_r($data->all());
+        // \App\Models\User::factory(10)->create();
     }
 }
