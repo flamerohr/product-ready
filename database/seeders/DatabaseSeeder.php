99cdb869c3aeb8929d1b00a39e7d143d9d67a5b1
Chris Joe <chris.joe@soulmachines.com>
2021-02-08 17:13:35 +1300

    feat: update seeder to save transaction data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Spatie\SimpleExcel\SimpleExcelReader;
 use Carbon\Carbon;
+use App\Models\Transaction;
+use App\Services\TransactionService;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,26 +15,34 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(TransactionService $transactionService)
     {
         // TODO: look for a better way to form a relative path name
         $movementPath = dirname(__FILE__) . '/sources/Fertiliser inventory movements - Sheet1.csv';
         $rows = SimpleExcelReader::create($movementPath)->getRows();
 
-        $data = $rows->map(function(array $props) {
-            // TODO: maybe use Carbon::createFromFormat() in future
-            // to make sure DateTime understands the format is d/m/y format
-            $date = new Carbon(str_replace('/', '-', $props['Date']));
+        $transactions = $rows
+            ->map(function (array $props) {
+                // TODO: maybe use Carbon::createFromFormat() in future
+                // to make sure DateTime understands the format is d/m/y format
+                $date = new Carbon(str_replace('/', '-', $props['Date']));
+                $price = $props['Unit Price'];
 
-            return [
-                'date' => $date,
-                'type' => $props['Type'],
-                'quantity' => $props['Quantity'],
-                'unit_price' => $props['Unit Price'],
-            ];
-        });
+                return [
+                    'date' => $date,
+                    'type' => $props['Type'],
+                    'quantity' => $props['Quantity'],
+                    'unit_price' => is_numeric($price) ? $price : 0,
+                ];
+            })
+            // TODO: improve way to process transactions
+            ->map(function ($data) {
+                $transaction = Transaction::create($data);
+                $transaction->save();
 
-        print_r($data->all());
-        // \App\Models\User::factory(10)->create();
+                return $transaction;
+            })
+
+            print_r($transactions->all());
     }
 }
